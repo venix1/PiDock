3665d48a64f3e77d4d3306bd1deb9e7e238cc8d3
Daniel Green <venix1@gmail.com>
2016-04-07 17:17:34 -0500

    Add module name to setup.py

--- PyVNC/setup.py
+++ PyVNC/setup.py
@@ -1,10 +1,14 @@
+"""Distutils setup.py for PyVNC."""
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize([
+    name='PyVNC',
+    ext_modules=cythonize([
         Extension("rfb", ["rfb.pyx"],
-                  libraries=["vncserver", 'vncclient'])
-    ])
+                  libraries=["vncserver", 'vncclient'],
+                  extra_compile_args=["-g"],
+                  extra_link_args=["-g"])
+    ], gdb_debug=True)
 )
