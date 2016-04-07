78aa11a3cf5fbc62cdcd8e1146a75f0b8a8ed8d4
Daniel Green <venix1@gmail.com>
2016-04-07 17:22:59 -0500

    Update to match module specification.
    * Add constants for attributes and commands.
    * Rename PiDockConnection to PiDockSocket
    * Cleanup print commands
    * stub add_connector, remove_connector
    * getfb method to return output crtc primary framebuffer.
    * Drop support for TILE_RECT_DATA. Expected to use mmap.

--- pidock/nl.py
+++ pidock/nl.py
@@ -7,17 +7,33 @@ import struct
 # import logging
 # logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-from libnl.attr import nla_len
+from libnl.attr import nla_len, nla_put_u32, nla_get_u32
 from libnl.errno_ import NLE_AGAIN
 from libnl.handlers import NL_CB_CUSTOM, NL_CB_SEQ_CHECK, NL_CB_VALID, NL_OK
-from libnl.msg import nlmsg_hdr
-from libnl.nl import nl_recvmsgs_default
+from libnl.msg import nlmsg_hdr, nlmsg_alloc
+from libnl.nl import nl_recvmsgs_default, nl_send_auto, NL_AUTO_PORT
+from libnl.nl import NL_AUTO_SEQ
 from libnl.socket_ import nl_socket_add_memberships, nl_socket_alloc
 from libnl.socket_ import nl_socket_modify_cb
 from libnl.genl.ctrl import genl_ctrl_resolve, genl_ctrl_resolve_grp
-from libnl.genl.genl import genl_connect, genlmsg_parse
+from libnl.genl.genl import genl_connect, genlmsg_parse, genlmsg_put
+from libnl.genl.genl import genlmsg_hdr
 
-from event import EventManager
+from .event import EventManager
+
+PIDOCK_A_UNSPEC = 0
+PIDOCK_A_TILE_RECT = 1
+PIDOCK_A_TILE_DATA = 2
+PIDOCK_A_OUTPUT_ID = 3
+PIDOCK_A_FB_ID = 4
+
+PIDOCK_A_MAX = 4
+
+PIDOCK_C_FB_DIRTY = 0
+PIDOCK_C_FB_REFRESH = 1
+PIDOCK_C_CONNECTOR_ADD = 2
+PIDOCK_C_CONNECTOR_REMOVE = 3
+PIDOCK_C_GETFB = 4
 
 
 def noop_seq_check(msg, _):
@@ -25,7 +41,7 @@ def noop_seq_check(msg, _):
     return NL_OK
 
 
-class Connector(object):
+class PiDockConnector(object):
     """Proxy to kernel pidock_connector structure."""
 
     def __init__(self):
@@ -33,7 +49,7 @@ class Connector(object):
         pass
 
 
-class PiDockConnection(object):
+class PiDockSocket(object):
     """Proxy to kernel pidock_connection structure."""
 
     def __init__(self, framebuffer):
@@ -51,9 +67,9 @@ class PiDockConnection(object):
         rc = genl_connect(sk)
         assert rc == 0, 'Failed to connect socket'
 
-        family = genl_ctrl_resolve(sk, b'pidock')
-        group = genl_ctrl_resolve_grp(sk, b'pidock', b'pidock_mc_group')
-        print('family: %d\tgroup: %d' % (family, group))
+        self.family = genl_ctrl_resolve(sk, b'pidock')
+        self.group = genl_ctrl_resolve_grp(sk, b'pidock', b'pidock_mc_group')
+        print('family: %d\tgroup: %d' % (self.family, self.group))
 
         # Set buffers to handle large payloads
         sk.socket_instance.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF,
@@ -62,7 +78,7 @@ class PiDockConnection(object):
 
         # nl_socket_set_nonblocking(sk)
         sk.socket_instance.setblocking(0)
-        nl_socket_add_memberships(sk, group, 0)
+        nl_socket_add_memberships(sk, self.group, 0)
 
     def run_once(self):
         """Poll netlink queue for new messages."""
@@ -79,7 +95,29 @@ class PiDockConnection(object):
 
     def add_connector(self, connector):
         """Send netlink message to add connector."""
-        pass
+        msg = nlmsg_alloc()
+        genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, self.family,
+                    0, 0, PIDOCK_C_CONNECTOR_ADD, 0)
+
+        nla_put_u32(msg, PIDOCK_A_OUTPUT_ID, 1337)
+
+        rc = nl_send_auto(self.sk, msg)
+        if rc:
+            assert False, rc
+
+    def getfb(self, output):
+        """Get associated framebuffer id for output."""
+        msg = nlmsg_alloc()
+        genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, self.family,
+                    0, 0, PIDOCK_C_GETFB, 0)
+
+        nla_put_u32(msg, PIDOCK_A_OUTPUT_ID, 0)
+        rc = nl_send_auto(self.sk, msg)
+
+        if rc < 0:
+            raise Exception('failed')
+
+        # asyncio.Future
 
     def remove_connector(self, connector):
         """Send netlink message to remove connector."""
@@ -90,33 +128,29 @@ class PiDockConnection(object):
         """Receive and parse netlink message."""
         # print('Got Message')
         nlh = nlmsg_hdr(msg)
-        tb = [None] * 10
+        info = genlmsg_hdr(nlh)
+        cmd = info[0]
+
+        # print(cmd)
+        tb = [None] * (PIDOCK_A_MAX+1)
 
         # TODO: Expect multiple messages from libnl
 
-        rc = genlmsg_parse(nlh, 0, tb, 5, None)
+        rc = genlmsg_parse(nlh, 0, tb, PIDOCK_A_MAX, None)
         assert rc == 0, 'genlmsg_parse failed: ' + str(rc)
 
-        x, y, width, height, pitch = \
-            struct.unpack('IIIII', tb[2].payload[:nla_len(tb[2])])
-        # print('Dirty: (%d, %d)  (%d, %d) %d' % (x,y,width,height, pitch))
+        if cmd == PIDOCK_C_FB_DIRTY:
+            r_msg = tb[PIDOCK_A_TILE_RECT]
+            x, y, width, height, pitch = \
+                struct.unpack('IIIII', r_msg.payload[:nla_len(r_msg)])
+            fb_id = nla_get_u32(tb[PIDOCK_A_FB_ID])
 
-        data = tb[3].payload[:nla_len(tb[3])]
-        scanline = 1680*4
-        offset = scanline * y + x
+            EventManager.emit('fb/damage', (x, y, width, height))
+        elif cmd == PIDOCK_C_GETFB:
+            fb_id = nla_get_u32(tb[PIDOCK_A_FB_ID])
 
-        assert len(data) >= pitch, \
-            '%d %d %d %d %d' % (x, y, width, height, pitch)
-
-        for h in range(height):
-            line = pitch*h
-            # Using c_void_p type causes segfault!!
-            # dst = ctypes.c_void_p.from_address(fb + offset + line)
-            # src = ctypes.c_void_p.from_buffer(data[line:])
-            dst = fb + offset + scanline * h
-            src = ctypes.addressof(ctypes.c_void_p.from_buffer(data[line:]))
-            ctypes.memmove(dst, src, pitch)
-
-        EventManager.emit('fb/dirty', (x, y, width, height))
+            EventManager.emit('output/fb', fb_id)
+        else:
+            print('Cmd:', cmd)
 
         return NL_OK
